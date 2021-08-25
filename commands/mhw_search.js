6298b1160a715ef22fcf617ee48e0e472484e83d
neryss <neryssistos@gmail.com>
2021-08-25 15:02:05 +0000

    Goes through api instead of wiki

--- commands/mhw_search.js
+++ commands/mhw_search.js
@@ -4,16 +4,16 @@ const { formatWithOptions } = require("util");
 
 module.exports = function	mhw_search(msg, name)
 {
-	msg.channel.send(name[1]);
-	fetch("https://monsterhunterworld.wiki.fextralife.com/" + name[1])
+	fetch("https://mhw-db.com/ailments")
 	.then(function (response) {
-		return (response.text());
-	}).then(function (html) {
-		fs.writeFile("./test.txt", html, function(ferr) {
+		return (response.json());
+	}).then(function (data) {
+		const test = JSON.stringify(data, null, 4);
+		fs.writeFile("./test.txt", test, function(ferr) {
 			if (ferr)
 				return (console.log("oopsi : " + err));
 		});
-		console.log(html);
+		console.log(test);
 	}).catch(function (err) {
 		console.warn("Something went wrong : ", err);
 	});
