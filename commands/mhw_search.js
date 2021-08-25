94db9348143bffc07395076c68250eb6385345c2
neryss <neryssistos@gmail.com>
2021-08-25 18:50:26 +0000

    can find through array

--- commands/mhw_search.js
+++ commands/mhw_search.js
@@ -4,11 +4,19 @@ const { formatWithOptions } = require("util");
 
 module.exports = function	mhw_search(msg, name)
 {
-	fetch("https://mhw-db.com/ailments")
+	fetch("https://mhw-db.com/monsters")
 	.then(function (response) {
 		return (response.json());
 	}).then(function (data) {
-		const test = JSON.stringify(data, null, 4);
+		for (var i = 0; i < data.length; i++)
+		{
+			if (data[i].name == "Odogaron")
+			{
+				const test = JSON.stringify(data[i], null, 4);
+				console.log("HERE IT IS : " + test);
+				break;
+			}
+		}
 		fs.writeFile("./test.txt", test, function(ferr) {
 			if (ferr)
 				return (console.log("oopsi : " + err));
