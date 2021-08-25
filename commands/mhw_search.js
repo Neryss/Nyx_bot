613c4bd50f394448202bac7c9b356291a5bb54e2
neryss <neryssistos@gmail.com>
2021-08-25 03:03:44 +0000

    ugly ass result but it works

--- commands/mhw_search.js
+++ commands/mhw_search.js
@@ -1,12 +1,17 @@
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
+const fs = require("fs");
+const { formatWithOptions } = require("util");
 
 module.exports = function	mhw_search(msg)
 {
 	fetch("https://monsterhunterworld.wiki.fextralife.com/Monster+Hunter+World+Wiki")
 	.then(function (response) {
-		
 		return (response.text());
 	}).then(function (html) {
+		fs.writeFile("./test.txt", html, function(ferr) {
+			if (err)
+				return (console.log("oopsi : " + err));
+		});
 		console.log(html);
 	}).catch(function (err) {
 		console.warn("Something went wrong : ", err);
