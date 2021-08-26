5396f5650ff767f2a1a1da539075b4d84bdcc24f
neryss <neryssistos@gmail.com>
2021-08-26 02:48:49 +0000

    working for single word name

--- commands/mhw_search.js
+++ commands/mhw_search.js
@@ -4,6 +4,11 @@ const { formatWithOptions } = require("util");
 const { DiscordAPIError } = require("discord.js");
 const Discord = require("discord.js");
 
+function	put_star(nb)
+{
+	return (Array(nb).join("<:star:880281496315899955>"));
+}
+
 module.exports = function	mhw_search(msg, name)
 {
 	console.log("called search");
@@ -19,16 +24,20 @@ module.exports = function	mhw_search(msg, name)
 			{
 				weakness = new Discord.MessageEmbed()
 					.setColor("#ff0080")
-					.setTitle(data[i].name);
+					.setTitle(data[i].name)
+					.addField("Weaknesses :", "\u200b");
 				console.log("found " + data[i].name + " weaknesses : " + JSON.stringify(data[i].weaknesses, null, 4));
 				e_weak = data[i].weaknesses;
 				for (var j = 0; j < e_weak.length; j++)
 				{
-					weakness.addField(e_weak[j].element + " : ", e_weak[j].stars);
+					if (e_weak[j].stars > 1)
+						weakness.addField(e_weak[j].element + " : ", put_star(e_weak[j].stars), false);
 					console.log(j);
 				}
-				// test = JSON.stringify(data[i].weaknesses, null, 4);
-				console.log("JE SUIS LA");
+				weakness.setFooter(
+					"Nyx",
+					"https://cdn.discordapp.com/attachments/840208014722990080/845232845912145950/takane_enomoto_10229.jpeg",
+				);
 				msg.channel.send(weakness);
 				break;
 			}
