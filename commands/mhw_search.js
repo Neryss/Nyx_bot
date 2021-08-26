ff308fab1f9b0f61759fe53b9fdb4bfc2f4a8cc8
neryss <neryssistos@gmail.com>
2021-08-26 02:10:35 +0000

    Basics done, works for everyone

--- commands/mhw_search.js
+++ commands/mhw_search.js
@@ -4,23 +4,29 @@ const { formatWithOptions } = require("util");
 const { DiscordAPIError } = require("discord.js");
 const Discord = require("discord.js");
 
-weakness = new Discord.MessageEmbed()
-	.setColor("#ff0080");
 module.exports = function	mhw_search(msg, name)
 {
+	console.log("called search");
 	fetch("https://mhw-db.com/monsters")
 	.then(function (response) {
+		console.log("Fetch done");
 		return (response.json());
 	}).then(function (data) {
 		for (var i = 0; i < data.length; i++)
 		{
-			console.log("name : " + name[1].toLowerCase());
+			console.log("i is ; " + i);
 			if (data[i].name.toLowerCase() == name[1].toLowerCase())
 			{
-				console.log(data[i].weaknesses);
+				weakness = new Discord.MessageEmbed()
+					.setColor("#ff0080")
+					.setTitle(JSON.stringify(data[i].name));
+				console.log("found " + data[i].name + " weaknesses : " + JSON.stringify(data[i].weaknesses, null, 4));
 				e_weak = data[i].weaknesses;
-				for (var j = 0; j < e_weak.length; i++)
-					weakness.addField(e_weak[j].element);
+				for (var j = 0; j < e_weak.length; j++)
+				{
+					weakness.addField(JSON.stringify(e_weak[j].element) + " : " + JSON.stringify(e_weak[j].stars));
+					console.log(j);
+				}
 				// test = JSON.stringify(data[i].weaknesses, null, 4);
 				console.log("JE SUIS LA");
 				msg.channel.send(weakness);
@@ -28,7 +34,7 @@ module.exports = function	mhw_search(msg, name)
 				break;
 			}
 		}
-		fs.writeFile("./test.txt", test, function(ferr) {
+		fs.writeFile("./test.txt", JSON.stringify(e_weak, null, 4), function(ferr) {
 			if (ferr)
 				return (console.log("oopsi : " + err));
 		});
