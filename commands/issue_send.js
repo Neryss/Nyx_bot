ec370fc366ae1d2a1d85900ddd87b19863b2d284
neryss <neryssistos@gmail.com>
2021-08-28 04:23:37 +0000

    issue working

--- commands/issue_send.js
+++ commands/issue_send.js
@@ -4,10 +4,11 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("issue")
 		.setDescription("reports an issue")
-		.addStringOption(option => option.setName('input').setDescription('Enter a string')),
+		.addStringOption(option => option.setName('input').setDescription('Issue to report')),
 	async execute (interaction) {
 		const issue = interaction.options.getString('input');
 		console.log(issue);
-		client.users.cache.get(process.env.MY_ID).send("<@" + interaction.user.tag + "> has reported an issue!\n\"" + issue + "\"");
+		client.users.cache.get(process.env.MY_ID).send("<@" + interaction.user.id + "> has reported an issue!\n\"" + issue + "\"");
+		await interaction.reply({content: "Issue sent !", ephemeral: true});
 	}
 }
