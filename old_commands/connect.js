feff1473f1b4f39728ccee981f8b9e859c641d8f
neryss <neryssistos@gmail.com>
2021-09-06 11:13:45 +0000

    can connect to channel

--- old_commands/connect.js
+++ old_commands/connect.js
@@ -3,14 +3,15 @@ const { channel } = require('diagnostics_channel');
 // const { client } = require('../index.js')
 
 module.exports = async function connect() {
-	const guild = client.guilds.cache.get('838851865432686604');
-	console.log(guild.id);
 	if (global.toggle)
 	{
+		const channel = client.channels.cache.get('838851865432686604');
+		console.log(channel.id);
 		const connection = joinVoiceChannel({
-			channelId: '838851865432686604',
-			guildId: '838851863969005619',
-			// adapterCreator: client.guild.
+			channelId: channel.id,
+			guildId: channel.guild.id,
+			adapterCreator: channel.guild.voiceAdapterCreator
 		});
+
 	}
 }
