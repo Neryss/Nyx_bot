dff2ffac91c45ccedceb6398a71d4d58a948515d
neryss <neryssistos@gmail.com>
2021-12-29 23:52:42 +0000

    meow the cat

---
+++ commands/cat.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const https = require('https');
+const axios = require('axios').default;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cat')
+		.setDescription('Get the meow mewo'),
+	async execute(interaction) {
+		let response = await axios.get("https://api.thecatapi.com/v1/images/search")
+		console.log(response.data[0].url);
+		await interaction.reply(response.data[0].url);
+	},
+};
