41d669a99b7bc4d88e4e54ef84e1c4e42d366a98
neryss <neryssistos@gmail.com>
2021-12-30 00:06:04 +0000

    fixed typo

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ const axios = require('axios').default;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('cat')
-		.setDescription('Get the meow mewo'),
+		.setDescription('Get the meow meow'),
 	async execute(interaction) {
 		let response = await axios.get("https://api.thecatapi.com/v1/images/search")
 		console.log(response.data[0].url);
